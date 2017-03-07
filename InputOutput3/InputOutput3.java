102b7371420ecf9f01ed77b87cc965b29be91dd0
Iman firmansyah <bwekz.pirmansyah@gmail.com>
2017-03-08 00:07:27 +0700

    Add files via upload

---
+++ InputOutput3/InputOutput3.java
@@ -0,0 +1,12 @@
+package inputoutput;
+
+import java.until.Scanner;
+
+Public class InputOutput3 {
+	public static void main(String[] args){
+		Scanner input = new Scanner (System.in);
+		String nama;
+		
+		nama = input.nextLine();
+	}
+}
